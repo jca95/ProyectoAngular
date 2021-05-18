9d136d5d64ada1263a8459a08d359b008caf1717
Jose Conejo <conejin1995@gmail.com>
2021-05-18 15:44:11 +0200

    Material Angular and Firebase install

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCzVdJwR3ancslRIo3iEpwOJOxhAbDX4Rc",
+    authDomain: "banda-b577f.firebaseapp.com",
+    projectId: "banda-b577f",
+    storageBucket: "banda-b577f.appspot.com",
+    messagingSenderId: "271842560965",
+    appId: "1:271842560965:web:b724f1a52d818d0700d9ac",
+    measurementId: "G-1PD31C8M8V"
+  }
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
